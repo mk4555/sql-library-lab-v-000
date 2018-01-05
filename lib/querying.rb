db7961046998b4ab13c11f760d55b1725f788743
Min Kim <mk4555@nyu.edu>
2018-01-05 00:21:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
   FROM characters
-  WHERE MAX(length(characters.motto))
+  HAVING MAX(length(characters.motto))
   "
 end
 
