d9100db38e3e170b0ab1feff4951e451cf54fa9d
Min Kim <mk4555@nyu.edu>
2018-01-05 00:37:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  JOIN characters ON series.id = characters.id
+  WHERE characters.species = 'human'
+  GROUP BY characters.species
+  ORDER BY MAX(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
