907e6e5d907ba982a1484d5cd6390b2a20155669
Min Kim <mk4555@nyu.edu>
2018-01-05 00:21:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,8 +7,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, MAX(length(characters.motto))
+  "SELECT characters.name, characters.motto
   FROM characters
+  WHERE MAX(length(characters.motto))
   "
 end
 
